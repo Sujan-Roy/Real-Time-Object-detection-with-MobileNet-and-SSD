6ed16dcefb196d7dcf20aa8c20593d8c39dbbd82
Sujan Chandra Roy <sujan.007.ice@gmail.com>
2023-05-18 10:31:09 +0900

    Update real_time_object_detection.py

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -1,6 +1,4 @@
-# Run This Commmand python real_time_object_detection.py 
-### --prototxt MobileNetSSD_deploy.prototxt.txt 
-### --model MobileNetSSD_deploy.caffemodel
+# Run This Commmand python real_time_object_detection.py  --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 
 # import the necessary packages
 from imutils.video import VideoStream
